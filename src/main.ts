1e2d87eb1c072c7d3320f4e6645bf372cc89d06f
Ruslan <Giovingo92@gmail.com>
2025-04-01 20:39:56 +0300

    feat: absolute paths

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  if (!process.env.IS_TS_NODE) {
+    await import('module-alias/register');
+  }
+
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3030);
 }
