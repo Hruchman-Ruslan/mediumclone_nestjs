d93d116efaaf3c4e6986523b031c028067bcd477
Ruslan <Giovingo92@gmail.com>
2025-04-22 17:51:38 +0300

    feat: update current user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,6 +5,7 @@ import {
   HttpException,
   HttpStatus,
   Post,
+  Put,
   Req,
   UseGuards,
   UsePipes,
@@ -18,6 +19,7 @@ import { ExpressRequestInterface } from '@app/types/expressRequest.interface';
 import { User } from './decorators/user.decorator';
 import { UserEntity } from './user.entity';
 import { AuthGuard } from './guards/auth.guard';
+import { UpdateUserDto } from './dto/updateUser.dto';
 
 @Controller()
 export class UserController {
@@ -46,6 +48,20 @@ export class UserController {
   async getCurrentUser(
     @User() user: UserEntity,
   ): Promise<UserResponseInterface> {
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Put('user')
+  @UseGuards(AuthGuard)
+  async updateCurrentUser(
+    @User('id') currentUserId: number,
+    @Body('user') updateUserDto: UpdateUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.updateUser(
+      currentUserId,
+      updateUserDto,
+    );
+
     return this.userService.buildUserResponse(user);
   }
 }
